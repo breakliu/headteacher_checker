1833355ff6c3bed815d22e79a4f8b2ba17472a8c
imlcl <slackcode@gmail.com>
2012-10-19 10:40:00 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadteacherChecker::Application.config.secret_token = 'c95db6a43320b966d038ae9ea4531fb18a4f464db0ac49464bd78ba9be8409458f87a40a9f266273f35f4e421707803bcdc38ec8184c94f224d78c3a8d38db27'
